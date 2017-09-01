6fbe193f1eb27691551713254831bdf854c2d4b1
P041270 <P041270@TCIP041270>
2017-09-01 10:43:31 +0900

    new file

---
+++ gs-rest-service-master/complete/src/main/java/hello/ChainCodeReturnParam.java
@@ -0,0 +1,33 @@
+package hello;
+
+public class ChainCodeReturnParam {
+
+	private String blckCustNum = "";
+	private String balance = "";
+	private String returnCode = "";
+	
+	ChainCodeReturnParam(String blckCustNum, String balance, String returnCode  ){
+		this.blckCustNum = blckCustNum;
+		this.balance = balance;
+		this.returnCode = returnCode;
+	}
+	
+	public String getBlckCustNum() {
+		return blckCustNum;
+	}
+	public void setBlckCustNum(String blckCustNum) {
+		this.blckCustNum = blckCustNum;
+	}
+	public String getBalance() {
+		return balance;
+	}
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
+	public String getReturnCode() {
+		return returnCode;
+	}
+	public void setReturnCode(String returnCode) {
+		this.returnCode = returnCode;
+	}
+}
